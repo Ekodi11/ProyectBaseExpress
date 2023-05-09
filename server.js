528a8feb347e2935d491b5ca314cad9a2adccb31
ThiagoC <tmcaballeroerfurt@escuelasproa.edu.ar>
2023-05-09 12:25:51 -0300

    conneccion con mongodb-atlas

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 const dotenv = require("dotenv");
+const uri = "mongodb+srv://tmcaballeroerfurt:Friv1234@feriadeciencias.30inxeo.mongodb.net/?retryWrites=true&w=majority"
 /*
 Carga de variables de entorno
 dotenv.config({ path: "./config.env" });
  const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
-
-Conexión al cloud de Mongodb Atlas
- mongoose
-     .connect(DB, {
-         useNewUrlParser: true,
-     })
-     .then((con) => {
-         //console.log(con.connections);
-         console.log("Connected to database");
-     });
 */
+
+function connect() {
+	mongoose
+		.connect(uri)
+		.then(() => {
+			console.log('Connected to the database ');
+		})
+		.catch((err) => {
+			console.error(`Error connecting to the database. n${err}`);
+		});
+}
+
 const port = 3000;
 //Corremos el servidor en el puerto seleccionado
 app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port} correctamente`);
 });
+ 
